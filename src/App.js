1aa107485ddcdfc5ae48d983c7ba38169369b724
Ishan <ishanwijes@gmail.com>
2023-05-31 23:02:23 +1000

    completed sass with react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import MainPage from './components/MainPage'
 
 export default function App() {
   return (
     <div>
       <Navbar />
+
+      <MainPage />
     </div>
   )
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/MainPage.css'
+
+export default function MainPage() {
+  return (
+    <div className='mainpage'>
+      <h1>Welcome to the Website</h1>
+
+      <button>Click Here</button>
+    </div>
+  )
+}
